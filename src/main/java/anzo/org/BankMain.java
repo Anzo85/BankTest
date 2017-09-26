536e9c86d15d199d54ca4ea037d0da6e0484043f
Andrii Zolotarov <anzo0316@netcracker.com>
2017-09-26 17:49:35 +0300

    com02

--- src/main/java/anzo/org/BankMain.java
+++ src/main/java/anzo/org/BankMain.java
@@ -5,20 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class BankMain {
     public static void main(String[] args) throws IOException {
+        System.out.println("Banking system has been started");
 
         Card testCard = new Card(1234, "Test", 33000);
 
-
-        System.out.println("Banking system has been started");
+        List<Card> cards = new ArrayList<Card>();
+        cards.add(0, testCard);
 
         System.out.println("Insert your card!");
         BufferedReader set = new BufferedReader(new InputStreamReader(System.in));
         String key = set.readLine();
-        if (key.equals("Y")){
-            System.out.println(testCard);
+        if ( cards.isEmpty() == false && cards.toString().contains(key) ){
+            System.out.println(cards);
         } else {
 
             BufferedReader set1 = new BufferedReader(new InputStreamReader(System.in));
